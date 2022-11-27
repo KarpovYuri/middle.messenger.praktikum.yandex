97da3413347255a64b6561141b7749937e7ca174
Karpov Yuri <yurick@bk.ru>
2022-11-27 22:18:02 +0300

    feat: add class Block

---
+++ src/utils/Block.ts
@@ -0,0 +1,174 @@
+import { EventBus } from "./EventBus";
+import { nanoid } from "nanoid";
+
+class Block<TProps extends Record<string, any> = any> {
+  static EVENTS = {
+    INIT: "init",
+    FLOW_CDM: "flow:component-did-mount",
+    FLOW_CDU: "flow:component-did-update",
+    FLOW_RENDER: "flow:render"
+  } as const;
+
+  public id = nanoid(6);
+  protected props: TProps;
+  public children: Record<string, Block>;
+  private eventBus: () => EventBus;
+  private _element: HTMLElement | null = null;
+  private _meta: { tagName: string, props: TProps };
+
+  constructor(tagName = 'div', propsWithChildren: TProps) {
+    const eventBus = new EventBus();
+    const { props, children} = this._getChildrenAndProps(propsWithChildren);
+
+    this._meta = { tagName, props: props as TProps };
+    this.children = children;
+    this.props = this._makePropsProxy(props);
+    this.eventBus = () => eventBus;
+    this._registerEvents(eventBus);
+
+    eventBus.emit(Block.EVENTS.INIT);
+  }
+
+  private _getChildrenAndProps(childrenAndProps: TProps): { props: TProps, children: Record<string, Block>} {
+    const props: Record<string, unknown> = {};
+    const children: Record<string, Block> = {};
+
+    Object.entries(childrenAndProps).forEach(([key, value]) => {
+      if (value instanceof Block) {
+        children[key as string] = value;
+      } else {
+        props[key] = value;
+      }
+    });
+
+    return { props: props as TProps, children };
+  }
+
+  private _addEvents() {
+    const {events = {}} = this.props as TProps & {events: Record<string, () => void>};
+
+    Object.keys(events).forEach(eventName => {
+      this._element?.addEventListener(eventName, events[eventName]);
+    });
+  }
+
+  private _registerEvents(eventBus: EventBus) {
+    eventBus.on(Block.EVENTS.INIT, this.init.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_CDM, this._componentDidMount.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_CDU, this._componentDidUpdate.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_RENDER, this._render.bind(this));
+  }
+
+  private _createResources() {
+    const { tagName } = this._meta;
+    this._element = this._createDocumentElement(tagName);
+  }
+
+  private _init() {
+    this._createResources();
+    this.init();
+    this.eventBus().emit(Block.EVENTS.FLOW_RENDER);
+  }
+
+  protected init() {}
+
+  private _componentDidMount() {
+    this.componentDidMount();
+  }
+
+  protected componentDidMount() {}
+
+  public dispatchComponentDidMount() {
+    this.eventBus().emit(Block.EVENTS.FLOW_CDM);
+    Object.values(this.children).forEach(child => child.dispatchComponentDidMount());
+  }
+
+  private _componentDidUpdate(oldProps: TProps, newProps: TProps) {
+    if(this.componentDidUpdate(oldProps, newProps)) {
+      this.eventBus().emit(Block.EVENTS.FLOW_RENDER);
+    }
+  }
+
+  protected componentDidUpdate(oldProps: TProps, newProps: TProps) {
+    return true;
+  }
+
+  public setProps = (nextProps: any) => {
+    if (!nextProps) return;
+    Object.assign(this.props, nextProps);
+  }
+
+  public get element() {
+    return this._element;
+  }
+
+  private _render() {
+    const fragment = this.render();
+    this._element!.append(fragment);
+    this._addEvents();
+  }
+
+
+  protected render(): DocumentFragment {
+    return new DocumentFragment();
+  }
+
+  protected compile(template: (context: any) => string, context: any) {
+    const contextAndStubs = { ...context };
+
+    Object.entries(this.children).forEach(([name, component]) => {
+      contextAndStubs[name] = `<div data-id="${component.id}"></div>`;
+    });
+
+    const html = template(contextAndStubs);
+    const temp = document.createElement('template');
+    temp.innerHTML = html;
+
+    Object.entries(this.children).forEach(([_, component]) => {
+      const stub = temp.content.querySelector(`[data-id="${component.id}"]`);
+      if (!stub) return;
+      component.getContent()?.append(...Array.from(stub.childNodes));
+      stub.replaceWith(component.getContent()!);
+    });
+
+    return temp.content;
+  }
+
+  public getContent() {
+    return this.element;
+  }
+
+  private _makePropsProxy(props: any) {
+    const self = this;
+
+    return new Proxy(props, {
+      get(target, prop: string) {
+        const value = target[prop];
+        return typeof value === 'function' ? value.bind(target) : value;
+      },
+      set(target, prop: string, value) {
+        const oldTarget = {...target};
+        target[prop as keyof TProps] = value;
+        self.eventBus().emit(Block.EVENTS.FLOW_CDU, oldTarget, target );
+        return true;
+      },
+      deleteProperty() {
+        throw new Error("Нет доступа");
+      }
+    });
+  }
+
+  private _createDocumentElement(tagName: string) {
+    return document.createElement(tagName);
+  }
+
+  public show() {
+    this.getContent()!.style.display = "block";
+  }
+
+  public hide() {
+    this.getContent()!.style.display = "none";
+  }
+}
+
+export default Block;
--- src/utils/EventBus.ts
+++ src/utils/EventBus.ts
@@ -1,42 +1,30 @@
-// Класс шины событий
+export class EventBus {
 
-class EventBus {
-
-  // Объект событий
   private readonly listeners: Record<string, ((...args: unknown[]) => void)[]> = {};
 
-  // Подписка на событие
-  on(event: string, callback: () => void) {
-    // Проверка события
+  on(event: string, callback: () => void): void {
     if (!this.listeners[event]) {
       this.listeners[event] = [];
     }
-    // Добавление события
     this.listeners[event].push(callback);
   }
 
-  // Отписка от события
-  off(event: string, callback: () => void) {
+  off(event: string, callback: () => void): void {
     this._checkEvent(event);
-    // Удаление события
     this.listeners[event] = this.listeners[event]
       .filter(listener => listener !== callback);
   }
 
-  // Запуск события
-  emit(event: string, ...args: unknown[]) {
+  emit(event: string, ...args: unknown[]): void {
     this._checkEvent(event);
     this.listeners[event].forEach(listener => {
       listener(...args);
     });
   }
 
-  // Проверка события
   _checkEvent(event: string) {
     if (!this.listeners[event]) {
       throw new Error(`Нет события: ${event}`);
     }
   }
 }
-
-export default EventBus;
