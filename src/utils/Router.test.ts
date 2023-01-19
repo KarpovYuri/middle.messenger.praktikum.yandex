ad087e787f60fb9d0eb4171acdb91b7d024f36ab
Karpov Yuri <yurick@bk.ru>
2023-01-20 01:06:40 +0300

    Добавить: тест Router.ts

---
+++ src/utils/Router.test.ts
@@ -0,0 +1,49 @@
+import Router, { BlockConstructable } from './Router'
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+describe('Router', () => {
+
+  global.window.history.back = () => {
+    if (typeof window.onpopstate === 'function') {
+      window.onpopstate({currentTarget: window} as unknown as PopStateEvent);
+    }
+  };
+  global.window.history.forward = () => {
+    if (typeof window.onpopstate === 'function') {
+      window.onpopstate({currentTarget: window} as unknown as PopStateEvent);
+    }
+  }
+
+  const getContentFake = sinon.fake.returns(document.createElement('div'));
+
+  const BlockMock = class {
+    getContent = getContentFake;
+  } as unknown as BlockConstructable;
+
+  it('use() должен вернуть экземпляр Router', () => {
+    const result = Router.use('/', BlockMock);
+
+    expect(result).to.eq(Router);
+  });
+
+  describe('.back()', () => {
+    it('должна быть переход назад по истории', () => {
+      Router
+        .use('/', BlockMock)
+        .start();
+
+      Router.back();
+
+      expect(getContentFake.callCount).to.eq(1);
+    });
+  });
+
+  it('должна отображаться стартовая страница', () => {
+    Router
+      .use('/', BlockMock)
+      .start();
+
+    expect(getContentFake.callCount).to.eq(1);
+  });
+});
